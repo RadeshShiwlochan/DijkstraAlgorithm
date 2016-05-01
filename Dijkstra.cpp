24db456074e1bcf6bc3f4cdf815c9dbc90330aa9
Radesh Shiwlochan <radesh0430@gmail.com>
2016-05-01 00:38:00 -0400

    Fixed bestCostArray , need to fix CostMatrix

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class Dijkstra {
-   private:
+   public:
       int numNodes;
       int sourceNode;
       int minNode;
@@ -14,22 +14,20 @@ class Dijkstra {
       int* markedArray;
       int* bestCostArray;
 
-   public:
-
    	 Dijkstra(int amntOfNodes) {
    	 	numNodes = amntOfNodes;
    	 	costMatrix = new int*[19];
+      fatherArray = new int[amntOfNodes];
+      bestCostArray = new int[amntOfNodes];
+      markedArray   = new int[amntOfNodes];
    	 	for(int i = 0; i < 19; ++i)
    	 		costMatrix[i] = new int[19]();
-   	 	bestCostArray = new int[amntOfNodes];
-   	 	markedArray   = new int[amntOfNodes];
-   	 	bestCostArray = new int[amntOfNodes];
    	 	sourceNode = 1;
 
    	 	for(int i = 0; i < amntOfNodes; ++i) {
    	 		bestCostArray[i] = 9999;
    	 		markedArray[i] = 0;
-   	 		bestCostArray[i] = i;
+   	 		fatherArray[i] = i;
    	 	}
    	 }
 
@@ -38,23 +36,51 @@ class Dijkstra {
         while(readFromFile >> rowValue >> colValue >> costValue) {
             costMatrix[rowValue][colValue] = costValue;
         }
+        
+        cout << endl;
+        loadBestCostArray(1);
         debugPrint();
       }
 
       void debugPrint() {
+        cout << "This is CostMatrix " << endl;
       	for(int row = 0; row < 19; ++row) {
       		for(int col = 0; col < 19; ++col) {
       			if(costMatrix[row][col] != 0) 
       				cout << row << " " << col << " " << costMatrix[row][col] << endl;
       		}
       	}
+        cout << "This is bestCostArray" << endl;
+        for(int k = 0; k < numNodes; k++) 
+          cout << bestCostArray[k] << "  ";
+        cout << endl;
+        
+        cout << "This is fatherArray" << endl; 
+        for(int k = 0; k < numNodes; k++) 
+          cout << fatherArray[k] << "  ";
+        cout << endl;
+
+        cout << "This is markedArray" << endl;
+        for(int k = 0; k < numNodes; k++) 
+          cout << markedArray[k] << "  ";
+        cout << endl;
+
       }
 
-      void loadBestCostArray(int srcNode) {
-        //how to load the best cost matrix
+      //how to load the best cost matrix
         //find all the nodes that a node connects to 
         //load the value into the array 
-      	
+
+      void loadBestCostArray(int src) {
+        int counter = 0;
+        for(int i = 0; i < 19; i++) {
+          for(int j = 0; j < 19; j++) {
+            if(src == i) {
+              bestCostArray[counter++] = costMatrix[i][j];
+              cout << costMatrix[i][j] << endl;
+            }  
+          }
+        } 
       }    	
 };
 
